daa6ba9219193290caaf7ac0e61b4cbd9b21c2c1
aevao <arsarsars906@gmail.com>
2025-01-14 18:45:21 +0300

    order button scss

---
+++ src/app/(lk)/order-summary/page.tsx
@@ -0,0 +1,11 @@
+
+const OrderSummary = () => {
+    return (
+      <div>
+        <h1>Order Summary</h1>
+        <p>Thank you for your order!</p>
+      </div>
+    );
+  };
+  
+  export default OrderSummary;
